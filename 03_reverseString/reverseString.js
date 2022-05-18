2ae0ed93cddfe706e79a835062be7a73cb17646f
ArturoVa <85954822+ArturoVa@users.noreply.github.com>
2022-05-18 00:28:46 -0500

    Add solution problem 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let container = string.split("");
+    container = container.reverse();
+    container = container.join("");
+    
+    return container
 };
 
 // Do not edit below this line
