da34762d48886779f8b4810e27348813d612d1cf
ArturoVa <85954822+ArturoVa@users.noreply.github.com>
2022-05-17 22:26:46 -0500

    Add solution to problem 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repetitionNumber) { 
+    let repetitionArray = []; // an array that will contain all the repetition 
+    if(repetitionNumber<0){
+        return 'ERROR'
+    }
+    for(let i= 0; i<repetitionNumber; i++){
+        repetitionArray[i] = string; 
+    }
+    return repetitionArray.join('');
 };
 
 // Do not edit below this line
