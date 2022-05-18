c6c17e79028ff1c8df4b659ed9d7a1f855fba4c9
ArturoVa <85954822+ArturoVa@users.noreply.github.com>
2022-05-18 16:38:48 -0500

    Add solution to problem 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray) {
+    //arguments in here, refers to the other arguments given to the function (not counting the inputArray)
 
-};
+    //enter a loop that starts from the second argument taken
+    for(let i = 1; i<arguments.length; i++){ 
+        
+        // enter another loop that loops through the elements inside inputArray
+        for(let j = 0; j< inputArray.length;j++ ){ 
 
+             //checks if the argument is equal to an element inside the inputArray 
+            if(arguments[i] === inputArray[j]){
+                inputArray.splice(j,1); //if true, removes the array
+            }
+            //otherwise, it doesn't do anything to the inputArray.
+        }
+    
+    }
+    return inputArray;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
