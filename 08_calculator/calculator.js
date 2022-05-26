5861f8b189e03b86bc8d7ed8fc5e0b33d6459426
ArturoVa <85954822+ArturoVa@users.noreply.github.com>
2022-05-25 23:45:40 -0500

    Add solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
 const add = function() {
+  let newVal =0
+  for(let i =0;i<arguments.length; i++){
+    newVal+= arguments[i];
+  }
+  return newVal;
 	
 };
 
 const subtract = function() {
-	
+	return arguments[0]-arguments[1];
 };
 
 const sum = function() {
+  return arguments[0].reduce((sum,elmnt)=>sum +elmnt, 0);
 	
 };
 
 const multiply = function() {
-
+  return arguments[0].reduce((sum,elmnt)=>sum*elmnt, 1);
 };
 
 const power = function() {
-	
+	return arguments[0]**arguments[1]
 };
 
 const factorial = function() {
-	
+  let factorialVal=1;
+	for(let i = 1; i<=arguments[0];i++){
+    factorialVal *= i
+  }
+  return factorialVal
 };
 
 // Do not edit below this line
